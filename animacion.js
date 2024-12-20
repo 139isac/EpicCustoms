4140d5f50935078dd94ec1a55d3b4d283cac9d4f
Isac <isac139@vidalibarraquer.net>
2024-12-20 21:36:38 +0100

    Update animacion.js

--- animacion.js
+++ animacion.js
@@ -23,3 +23,20 @@ window.addEventListener('scroll', checkSections);
 
 // Llamar a la función al cargar la página
 document.addEventListener('DOMContentLoaded', checkSections);
+
+
+
+
+
+// Mostrar el banner si no se ha aceptado la política de cookies
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem('cookies-accepted')) {
+        document.getElementById('cookie-banner').style.display = 'block';
+    }
+
+    // Aceptar las cookies
+    document.getElementById('accept-cookies').addEventListener('click', () => {
+        localStorage.setItem('cookies-accepted', 'true'); // Guardar la aceptación en el almacenamiento local
+        document.getElementById('cookie-banner').style.display = 'none'; // Ocultar el banner
+    });
+});
